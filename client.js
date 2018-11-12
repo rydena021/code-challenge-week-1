cee2b12d4bafd391d8d72d7526d28d9066e187ab
Anders Ryden <rydena021@gmail.com>
2018-11-12 13:53:06 -0600

    swaps background color or deletes on click

--- client.js
+++ client.js
@@ -6,14 +6,35 @@ $(readyNow);
 function readyNow() {
   console.log('jq');
   $('#createButton').on('click', handleClick)
+  $('#contentDiv').on('click', '.swapButton', swapClick)
+  $('#contentDiv').on('click', '.deleteButton', deleteClick)
 }
 
 function handleClick() {
   console.log('in handleClick');
   clickCount += 1;
-  let divEl = $(`<div class="generatedDiv"></div>`);
+  let divEl = $(`<div class="generatedDiv" data-red="true"></div>`);
   $(divEl).append(`<p>${clickCount}</p>`);
-  $(divEl).append(`<button>Swap</button>`);
-  $(divEl).append(`<button>Delete</button>`);
+  $(divEl).append(`<button class="swapButton">Swap</button>`);
+  $(divEl).append(`<button class="deleteButton">Delete</button>`);
   $('#contentDiv').append(divEl);
+}
+
+function swapClick() {
+  console.log('in swapClick');
+  let div = this.closest('div');
+  let red = $(div).data('red');
+  if (red) {
+    $(div).removeClass('generatedDiv').addClass('swapDiv');
+    $(div).data("red", false);
+  } else {
+    $(div).removeClass('swapDiv').addClass('generatedDiv');
+    $(div).data("red", true);
+  }
+}
+
+function deleteClick() {
+  console.log('in deleteClick');
+  let div = this.closest('div');
+  $(div).remove();
 }
