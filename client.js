1a912576733fc1098416990ec02e2b22da4a4bd9
Anders Ryden <rydena021@gmail.com>
2018-11-12 13:30:26 -0600

    appends div on handleClick

--- client.js
+++ client.js
@@ -1,5 +1,19 @@
 console.log('js');
+let clickCount = 0;
+
 $(readyNow);
+
 function readyNow() {
   console.log('jq');
+  $('#createButton').on('click', handleClick)
+}
+
+function handleClick() {
+  console.log('in handleClick');
+  clickCount += 1;
+  let divEl = $(`<div class="generatedDiv"></div>`);
+  $(divEl).append(`<p>${clickCount}</p>`);
+  $(divEl).append(`<button>Swap</button>`);
+  $(divEl).append(`<button>Delete</button>`);
+  $('#contentDiv').append(divEl);
 }
